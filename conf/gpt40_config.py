612504b5cbe3d985f065216cfd2d0badac906fd9
shiracohen2 <shira.c93@gmail.com>
2025-05-13 14:51:36 +0300

    gpt40 conf

---
+++ conf/gpt40_config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+from dataclasses import dataclass
+from conf.base_gpt_config import BaseGptConfig
+
+load_dotenv()
+
+
+@dataclass
+class GPT4OConfig(BaseGptConfig):
+    api_key: str = os.getenv("GPT4O_KEY")
+    azure_endpoint: str = os.getenv("GPT4O_ENDPOINT")
+    vision_model_deployment_name: str = os.getenv("GPT4O_DEPLOYMENT_NAME")
