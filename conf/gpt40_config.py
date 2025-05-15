141468867cdcf04669b4579af1d71a267656777a
shiracohen2 <shira.c93@gmail.com>
2025-05-15 13:08:17 +0300

    fixed requirements.txt and gpt4o configurations

--- conf/gpt40_config.py
+++ conf/gpt40_config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from conf.base_gpt_config import BaseGptConfig
 
 load_dotenv()
@@ -12,3 +12,4 @@ class GPT4OConfig(BaseGptConfig):
     api_key: str = os.getenv("GPT4O_KEY")
     azure_endpoint: str = os.getenv("GPT4O_ENDPOINT")
     vision_model_deployment_name: str = os.getenv("GPT4O_DEPLOYMENT_NAME")
+    api_version: str = field(default="2025-01-01-preview")
